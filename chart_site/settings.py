21459d94f645796a93da28a154797cd7dab7ce32
Febin Firos <febinfiros74@gmail.com>
2021-08-09 14:15:03 +0300

    dirs

--- chart_site/settings.py
+++ chart_site/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'chart_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
