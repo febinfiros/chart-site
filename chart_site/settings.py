276f48f3c2efcbc8c0f86459450cc4660fd701f4
Febin Firos <febinfiros74@gmail.com>
2021-08-09 14:46:53 +0300

    secret key

--- chart_site/settings.py
+++ chart_site/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 import django_heroku
 import os
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
