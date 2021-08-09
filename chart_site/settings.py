670549f7dbc28be698c4f8c533836ddd97cbc3a9
Febin Firos <febinfiros74@gmail.com>
2021-08-09 11:27:37 +0300

    added django-heroku

--- chart_site/settings.py
+++ chart_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from django.core.management.utils import get_random_secret_key
 from pathlib import Path
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
